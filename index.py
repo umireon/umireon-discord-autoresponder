986a6b54d8a290c165f0e978d053ba9d114abfe3
Kaito Udagawa <umireon@gmail.com>
2021-09-27 02:53:04 +0900

    Update index.py

--- index.py
+++ index.py
@@ -13,7 +13,8 @@ Please ask participants for your raid to join WantsXxx tags. The instructions ar
 
 @client.event
 async def on_message(message):
-    uxie_role = client.get_role(885380248617758771)
+    server = client.get_guild(876131915424489472)
+    uxie_role = server.get_role(885380248617758771)
     if uxie_role in message.mentions: # 話しかけられたかの判定
         await reply(message) # 返信する非同期関数を実行
 
