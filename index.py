af1b4153ad44884984f845d0c797fc4a6fbc36ab
Kaito Udagawa <umireon@gmail.com>
2021-09-27 02:03:53 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,17 @@
+import discord
+import os
+
+client = discord.Client()
+
+TOKEN = os.environ.TOKEN
+
 async def reply(message):
-    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+    reply = f'Hey'
     await message.channel.send(reply) # 返信メッセージを送信
+
+@client.event
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+        await reply(message) # 返信する非同期関数を実行
+
+client.run(TOKEN)
