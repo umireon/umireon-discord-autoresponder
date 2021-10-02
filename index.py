c33972961baecdb663047ea86c08c41daa9cf7ef
Kaito Udagawa <umireon@gmail.com>
2021-10-02 14:45:10 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import re
 
 client = discord.Client()
 
@@ -9,28 +10,15 @@ async def reply(message):
     rules = client.get_channel(889512252472959018)
     reply = f"""Click the Create Thread in the context menu of the post (e.g. The image of egg) to create a new thread.
 Please ask participants for your raid to join WantsXxx tags. The instructions are in {rules.mention}."""
-    await message.channel.send(reply) # 返信メッセージを送信
+    await message.channel.send(reply)
 
 @client.event
 async def on_message(message):
     server = client.get_guild(876131915424489472)
-    mega_role = server.get_role(890106825817587723)
-    uxie_role = server.get_role(890106629830361099)
-    mesprit_role = server.get_role(890106668220821545)
-    azelf_role = server.get_role(890106033920442368)
-    threestar_role = server.get_role(890106737183572019)
-    onestar_role = server.get_role(890106793689231360)
-    if mega_role in message.role_mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
-    elif uxie_role in message.role_mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
-    elif mesprit_role in message.role_mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
-    elif azelf_role in message.role_mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
-    elif threestar_role in message.role_mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
-    elif onestar_role in message.role_mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
+    prog = re.compile("^Wants")
+    roles = set(role for role in server.roles if prog.match(role.name) is not None)
+    role_mentions = set(message.role_mentions)
+    if len(roles & role_mentions) > 0:
+        await reply(message)
 
 client.run(TOKEN)
