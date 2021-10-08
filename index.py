a2f346a9161e4d9b2c89dcf40f5b6b9a98e945fe
Kaito Udagawa <umireon@gmail.com>
2021-10-08 20:41:33 +0900

    Update index.py

--- index.py
+++ index.py
@@ -14,10 +14,8 @@ Please ask participants for your raid to join WantsXxx tags. The instructions ar
     await message.channel.send(reply)
 
 
-async def reply_one_three(message):
-    join_tags = client.get_channel(894244917184987226)
-    reply = f"""Click the Create Thread in the context menu of the post (e.g. The image of egg) to create a new thread.
-Please ask participants for your raid to join WantsXxx tags. The instructions are in {join_tags.mention}."""
+async def reply_one_three(one_three_role, message):
+    reply = f"""{one_three_role.mention}"""
     await message.channel.send(reply)
 
 
@@ -31,10 +29,11 @@ async def on_message(message):
     if len(roles & role_mentions) > 0:
         await reply_instructions(message)
 
+    one_three_role = server.get_role(893286398604501032)
     rufflet_role = server.get_role(893294731436625970)
     chansey_role = server.get_role(893487840133017661)
     one_three_roles = {rufflet_role, chansey_role}
     if len(one_three_roles & role_mentions) > 0:
-        await reply_one_three(message)
+        await reply_one_three(one_three_role, message)
 
 client.run(TOKEN)
