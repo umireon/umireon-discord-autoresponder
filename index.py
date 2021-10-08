12c0b9c9e9fcbceec8002a699985756ad958ee38
Kaito Udagawa <umireon@gmail.com>
2021-10-08 20:32:08 +0900

    Format

--- index.py
+++ index.py
@@ -6,11 +6,13 @@ client = discord.Client()
 
 TOKEN = os.environ['TOKEN']
 
+
 async def reply_instructions(message):
     join_tags = client.get_channel(894244917184987226)
     reply = f"""Click the Create Thread in the context menu of the post (e.g. The image of egg) to create a new thread.
 Please ask participants for your raid to join WantsXxx tags. The instructions are in {join_tags.mention}."""
     await message.channel.send(reply)
+
 
 async def reply_one_three(message):
     join_tags = client.get_channel(894244917184987226)
@@ -18,11 +20,13 @@ async def reply_one_three(message):
 Please ask participants for your raid to join WantsXxx tags. The instructions are in {join_tags.mention}."""
     await message.channel.send(reply)
 
+
 @client.event
 async def on_message(message):
     server = client.get_guild(876131915424489472)
     prog = re.compile("^Wants")
-    roles = set(role for role in server.roles if prog.match(role.name) is not None)
+    roles = set(role for role in server.roles if prog.match(
+        role.name) is not None)
     role_mentions = set(message.role_mentions)
     if len(roles & role_mentions) > 0:
         await reply_instructions(message)
