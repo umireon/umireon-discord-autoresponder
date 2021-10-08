57a54fb965f0fb4cebd8f89b01a3e0866f763418
Kaito Udagawa <umireon@gmail.com>
2021-10-08 20:31:15 +0900

    Update index.py

--- index.py
+++ index.py
@@ -6,10 +6,16 @@ client = discord.Client()
 
 TOKEN = os.environ['TOKEN']
 
-async def reply(message):
-    rules = client.get_channel(889512252472959018)
+async def reply_instructions(message):
+    join_tags = client.get_channel(894244917184987226)
     reply = f"""Click the Create Thread in the context menu of the post (e.g. The image of egg) to create a new thread.
-Please ask participants for your raid to join WantsXxx tags. The instructions are in {rules.mention}."""
+Please ask participants for your raid to join WantsXxx tags. The instructions are in {join_tags.mention}."""
+    await message.channel.send(reply)
+
+async def reply_one_three(message):
+    join_tags = client.get_channel(894244917184987226)
+    reply = f"""Click the Create Thread in the context menu of the post (e.g. The image of egg) to create a new thread.
+Please ask participants for your raid to join WantsXxx tags. The instructions are in {join_tags.mention}."""
     await message.channel.send(reply)
 
 @client.event
@@ -19,6 +25,12 @@ async def on_message(message):
     roles = set(role for role in server.roles if prog.match(role.name) is not None)
     role_mentions = set(message.role_mentions)
     if len(roles & role_mentions) > 0:
-        await reply(message)
+        await reply_instructions(message)
+
+    rufflet_role = client.get_role(893294731436625970)
+    chansey_role = client.get_role(893487840133017661)
+    one_three_roles = set(rufflet_role, chansey_role)
+    if len(one_three_roles & role_mentions) > 0:
+        await reply_one_three(message)
 
 client.run(TOKEN)
